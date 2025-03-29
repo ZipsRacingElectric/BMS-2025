eb7ce0773ffef57b720c2d45c6459393353a70e8
Cole Barach <colebarach@tutanota.com>
2025-03-29 12:57:14 -0400

    Tenative watchdog implementation.

---
+++ src/watchdog.c
@@ -0,0 +1,38 @@
+// Header
+#include "watchdog.h"
+
+void hardFaultCallback (void);
+
+// Configuration --------------------------------------------------------------------------------------------------------------
+
+static const WDGConfig WDG1_CONFIG =
+{
+	.pr		= 0b011,	// 1kHz
+	.rlr	= 1000		// 1s timeout at 1kHz
+};
+
+// Globals --------------------------------------------------------------------------------------------------------------------
+
+static bool triggerWatchdog = false;
+
+// Functions ------------------------------------------------------------------------------------------------------------------
+
+void watchdogStart ()
+{
+	// If the device was just reset due to the watchdog, enter the fault state.
+	if ((RCC->CSR & RCC_CSR_IWDGRSTF) == RCC_CSR_IWDGRSTF)
+		hardFaultCallback ();
+
+	wdgStart (&WDGD1, &WDG1_CONFIG);
+}
+
+void watchdogTrigger ()
+{
+	triggerWatchdog = true;
+}
+
+void watchdogReset ()
+{
+	if (!triggerWatchdog)
+		wdgReset (&WDGD1);
+}
